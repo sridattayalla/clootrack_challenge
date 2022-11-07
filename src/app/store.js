b46b433c36dd3cede983faeae62d9e13c29c8f82
Sri Datta <sridatta.yalla@juspay.in>
2022-11-07 16:17:04 +0530

    added zip

--- src/app/store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import dashboardReducer from './chartReducer'
-export default configureStore({
-    reducer: {
-        dashboard: dashboardReducer
-    },
-})
